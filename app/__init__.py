aad20b5c05612695e4d1e8951da96c2cc0d83974
Santiago Alvarez <salvarezd3@eafit.edu.co>
2024-12-11 14:14:37 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
+app = Flask(__name__)
+CORS(app)
 
-    # Registrar Blueprints
-    from app.routes.bg_remove_routes import bgRemove_bp
-    app.register_blueprint(bgRemove_bp)
+# Registrar Blueprints
+from app.routes.bg_remove_routes import bgRemove_bp
+app.register_blueprint(bgRemove_bp)
 
-    @app.route('/')
-    def home():
-        return "¡Bienvenido al Servicio de Remover Fondos!"
-
-    return app
-
-# Crear la instancia de la aplicación
-app = create_app()
+@app.route('/')
+def home():
+    return "¡Bienvenido al Servicio de Remover Fondos!"
