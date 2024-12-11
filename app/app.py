fd1fcaf894a4062955a0b7a8d7479f5882ef00cc
Santiago Alvarez <salvarezd3@eafit.edu.co>
2024-12-11 12:00:23 -0500

    fix deployment crash

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from routes.bg_remove_routes import remove_bg
+from routes.bg_remove_routes import bgRemove_bp
 
 app = Flask(__name__)
 
 # Registra las rutas
-app.register_blueprint(remove_bg)
+app.register_blueprint(bgRemove_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
